235aee7a57509a12c84df833b12467e279e327c0
perox1de <jan.kahnes2@gmail.com>
2022-01-25 00:50:08 +0100

    added comments

--- lexer.py
+++ lexer.py
@@ -12,7 +12,10 @@ FN_KEYWORD = "fn"
 # TODO: consider replacing confusing ifs with regex
 
 
+# Is responsible for generating a list of tokens based on a string input using the pre-defined Tokens
+# from the Tokens.py file.
 class Lexer:
+    # defines an iterator based on the string input
     def __init__(self, text):
         self.text = iter(text)
         self.current_char = None
@@ -28,17 +31,17 @@ class Lexer:
     # generates a list of tokens
     def gen_tokens(self):
         tokens = []
-
         while self.current_char is not None:
             if self.current_char == " ":
                 self.next_char()
-            elif self.current_char in NUM_CHARS:
+            elif self.current_char in (NUM_CHARS + "."):
                 tokens.append(self.gen_number())
             elif self.current_char in OPERATORS:
                 tokens.append(token(CHAR_TYPES.get(self.current_char)))
                 self.next_char()
             elif self.current_char == "=":
                 self.next_char()
+                # we have to differentiate between a simple equals and the function operator "=>", so we check for a ">"
                 if self.current_char == ">":
                     tokens.append(token(Tokens.FUNCTION_OPERATOR))
                     self.next_char()
@@ -46,6 +49,7 @@ class Lexer:
                     tokens.append(token(Tokens.EQUALS))
             elif self.current_char in LETTERS:
                 tokens.append(self.gen_identifier())
+                # will recognize when a function is called with a period after the identifier
                 if self.current_char == ".":
                     tokens.append(token(Tokens.PERIOD_FUNC_CALL))
                     self.next_char()
@@ -53,17 +57,21 @@ class Lexer:
                 raise Exception(f"Illegal Character {self.current_char}")
         return tokens
 
+    # will generate and return a number with multiple or one digit(s)
     def gen_number(self):
         number = ""
         while self.current_char is not None and self.current_char in (NUM_CHARS + "."):
             number += self.current_char
             self.next_char()
+        # will change ie. ".5" to "0.5"
         if number.startswith("."):
             number = "0" + number
+        # a number with multiple periods will raise an exception
         if number.count(".") > 1:
             raise Exception("Illegal number", number)
         return token(Tokens.NUMBER, float(number))
 
+    # will generate an identifier with multiple or one letter(s)
     def gen_identifier(self):
         name = ""
         while self.current_char is not None and self.current_char in (LETTERS + NUM_CHARS):
